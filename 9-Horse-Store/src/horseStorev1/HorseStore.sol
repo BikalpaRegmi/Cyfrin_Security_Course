ddd7d71fa2fa911b96e43dd0c755438ef5dcca67
Bikalpa Regmi <bikalparegmihtd3@gmail.com>
2025-07-03 12:46:55 +0545

    Started Yul & formal verification

---
+++ 9-Horse-Store/src/horseStorev1/HorseStore.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.27 ;
+
+contract HorseStore {
+
+    uint256 public horseNumber ;
+
+   function updateHorseNumber(uint256 _horseNum) external {
+    horseNumber = _horseNum ;
+   }
+
+   function getHorseNumber() external view returns(uint256){
+    return horseNumber ;
+   }
+}
